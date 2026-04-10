b74fc936deb9963c7b1a55d798ac99ef0d6b6144
surbhikashyap1692 <surbhi.kashyap1692@gmail.com>
2026-04-10 15:47:01 +0530

    Create main_test.py

---
+++ main_test.py
@@ -0,0 +1,13 @@
+import webtest
+
+import main
+
+
+def test_get():
+    app = webtest.TestApp(main.app)
+
+    response = app.get("/")
+
+    assert response.status_int == 200
+    assert response.body == "Hello, World!"
+  
